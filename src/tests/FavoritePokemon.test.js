4921ba35b2a8a164b2b2ec9786c5147e478d7553
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-11-23 14:50:33 -0300

    adiciona teste para o component favoritepokemon

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('teste se as informações do component é exibida', () => {
+  test('se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    userEvent.click(linkFavorite);
+    const title = screen.getByText(/no favorite pokémon found/i);
+    expect(title).toBeInTheDocument();
+  });
+});
